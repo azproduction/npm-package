4cc07a92cfd27341a9f3bcb726d67caaef7364a0
Mikhail Davydov <azazel.private@gmail.com>
2014-06-13 15:49:03 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl https://raw.github.com/azproduction/npm-package/master/init.sh > /tmp/npm-package-init.sh
+curl https://raw.githubusercontent.com/azproduction/npm-package/master/init.sh > /tmp/npm-package-init.sh
 sh /tmp/npm-package-init.sh < /dev/tty
 rm /tmp/npm-package-init.sh
